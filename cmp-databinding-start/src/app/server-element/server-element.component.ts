a35bc3fba2d78b12db5fb5feb8d3f9baca0887f7
konrad.plocharski <konrad.plocharski@spVM>
2020-06-12 16:48:55 +0000

    data binding

--- cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, AfterContentInit, Input, ViewChild, ElementRef, ContentChild } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.css']
 })
-export class ServerElementComponent implements OnInit {
-  @Input('srvElement') element: {type: string, name: string, content: string};
+export class ServerElementComponent implements OnInit, AfterContentInit {
+  @Input('srvElement') element: { type: string, name: string, content: string };
+  @ViewChild('heading', { static: true }) header: ElementRef;
+  @ContentChild('contentParagraph', { static: true }) paragraph: ElementRef;
   constructor() { }
 
   ngOnInit(): void {
+    console.log('text content' + this.header.nativeElement.textContent);
   }
 
+  ngAfterContentInit(): void {
+    console.log('paragraph content ' + this.paragraph.nativeElement.textContent);
+  }
 }
