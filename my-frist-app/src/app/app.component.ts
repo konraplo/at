6f348ad9c4ae9f4316815c742e78692614ac947e
konrad.plocharski <konrad.plocharski@spVM>
2020-06-16 22:02:36 +0000

    header events; refactoring

--- my-frist-app/src/app/app.component.ts
+++ my-frist-app/src/app/app.component.ts
@@ -8,4 +8,8 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'kpl frist-app';
   name = 'kpl';
+  loadedFeature: string = "recipe";
+  onNavigate(feature: string) {
+    this.loadedFeature = feature;
+  }
 }
